5020420ecaf57da6715d884e1a48a4a9913ff674
Po Tran <potran@Gauravs-iMac.local>
2018-07-18 16:11:39 +0800

    Initial commit

---
+++ app/src/main/java/com/example/potran/sampleprojectrecycleview/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.potran.sampleprojectrecycleview;
+
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference ref = database.getReference("message");
+
+        tv = (TextView)findViewById(R.id.textView);
+
+        ref.setValue("What's up homie");
+
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String value = dataSnapshot.getValue(String.class);
+                Toast.makeText(MainActivity.this, value, Toast.LENGTH_SHORT).show();
+                Log.d("Firebase value: ", value);
+                tv.setText(value);
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Toast.makeText(MainActivity.this, "Failed to read values", Toast.LENGTH_SHORT).show();
+                Log.w("Tazzzzz: ", "Failed to read");
+            }
+        });
+
+    }
+}
